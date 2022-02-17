0dcc0c86d68b0d6d0c3ff2ce51a8f79a4fc9d283
jikamad <99354694+jikamad@users.noreply.github.com>
2022-02-17 12:11:25 -0500

    Math Unit test (#1)

---
+++ specs/example.spec.js
@@ -0,0 +1,22 @@
+import {expect} from "chai";
+describe("Math functions", () => {
+
+
+
+
+    it('a+b=c', () => {
+let a = 1;
+let b = 2;
+let c = a + b;
+expect(c).to.eq(3)
+})
+
+
+it('a-b=c', () => {
+    let a = 1;
+    let b = 2;
+    let c = a - b;
+    expect(c).to.eq(-1)
+    })
+
+});
